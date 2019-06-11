13f232b2bf89f0ccb9e63ae1164eaf64085b61ff
stealthycoin <stealthycoin@users.noreply.github.com>
2019-06-10 17:45:24 -0700

    Initial commit

---
+++ puglyfe.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         PugLyfe
+// @version      0.1
+// @description  Get Dem Pugs
+// @author       You
+// @match        *://*/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle('html { background: none !important; }');
+    GM_addStyle('body { background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Pug_-_1_year_Old.jpg/1920px-Pug_-_1_year_Old.jpg"); !important; }');
+    GM_addStyle('body { background-size: contain; opacity: 0.6;}')
+})();
